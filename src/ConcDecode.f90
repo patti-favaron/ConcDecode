cdd679ffe9978a3dd5667a03c0904d130a2bea25
Patrizia Favaron <patti.favaron@gmail.com>
2023-03-24 10:19:11 +0100

    Update ConcDecode.f90

--- src/ConcDecode.f90
+++ src/ConcDecode.f90
@@ -1,13 +1,33 @@
 ! ConcDecode - Convert from CALPUFF current output file to "01" (ISC style) format.
 !
-! ConcDecode is copyrighted material.
+! =============================================================================
 !
-! Copyright (C) by Patrizia Favaron
+! MIT License
+!
+! Copyright (c) 2023 Patrizia Favaron
+!
+! Permission is hereby granted, free of charge, to any person obtaining a copy
+! of this software and associated documentation files (the "Software"), to deal
+! in the Software without restriction, including without limitation the rights
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is
+! furnished to do so, subject to the following conditions:
+!
+! The above copyright notice and this permission notice shall be included in all
+! copies or substantial portions of the Software.
+!
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+! SOFTWARE.
 !
 ! Major changes:
 ! ==============
 !
-! Version 1.00 by Patrizia Favaron (circa 2007)
+! Version 1.00 by Patrizia "Patti" Favaron (circa 2007)
 !
 ! 14. 07. 2017		The previous versions all assumed an unformatted
 !					CON file, produced with a compiler of which we had only
